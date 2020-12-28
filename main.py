d620c2e68fcf5a6a0b4c36b53a3fddd903973676
Heiner Büscher <heiner@buescher.de>
2020-12-28 16:51:16 +0100

    intial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# KnockOut Machine
+# Heiner Buescher, 28.12.2020
+
+
+# TODO add calculation
+def calculateTime():
+    return None
+
+
+# TODO get all the scores, sorted by highest
+def getScores():
+    return None
+
+
+def main():
+    print("Hello World!")
