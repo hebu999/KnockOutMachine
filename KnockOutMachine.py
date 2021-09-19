86aef8bae15305176c2e51cc4918f2e1f2c9a31e
Heiner Büscher <bestellungen_hb@outlook.de>
2021-09-19 15:49:18 +0200

    added another cancel button, replaced sound

--- KnockOutMachine.py
+++ KnockOutMachine.py
@@ -65,6 +65,9 @@ class Ui_MainWindow(object):
         self.input_window.setWindowTitle("Bitte Namen eingeben")
         self.input_window.resize(300, 100)
         self.input_window.move(850, 820)
+        # self.input_window.setWindowFlags(
+        #     QtCore.Qt.Window | QtCore.Qt.CustomizeWindowHint | QtCore.Qt.WindowStaysOnTopHint)
+
         self.input_layout = QtWidgets.QFormLayout()
 
         self.input_dialogue = QtWidgets.QLineEdit(self.centralwidget)
@@ -95,15 +98,23 @@ class Ui_MainWindow(object):
         self.highscoreButton.setFixedSize(191, 71)
         self.highscoreButton.clicked.connect(lambda: self.on_high_score_button_clicked())
 
-        self.cancelButton = QtWidgets.QPushButton(self.centralwidget)
-        self.cancelButton.setFont(buttonFont)
-        self.cancelButton.setObjectName("cancelButton")
-        self.cancelButton.setFixedSize(191, 71)
-        self.cancelButton.clicked.connect(lambda: self.exit_function())
-        self.cancelButton.hide()
+        self.cancelTimerButton = QtWidgets.QPushButton(self.centralwidget)
+        self.cancelTimerButton.setFont(buttonFont)
+        self.cancelTimerButton.setObjectName("cancelTimerButton")
+        self.cancelTimerButton.setFixedSize(191, 71)
+        self.cancelTimerButton.clicked.connect(lambda: self.exit_timer_function())
+        self.cancelTimerButton.hide()
+
+        self.cancelScoreButton = QtWidgets.QPushButton(self.centralwidget)
+        self.cancelScoreButton.setFont(buttonFont)
+        self.cancelScoreButton.setObjectName("cancelScoreButton")
+        self.cancelScoreButton.setFixedSize(191, 71)
+        self.cancelScoreButton.clicked.connect(lambda: self.exit_score_function())
+        self.cancelScoreButton.hide()
 
         self.lcdCounter = QtWidgets.QLCDNumber(self.centralwidget)
         self.lcdCounter.setEnabled(False)
+        self.lcdPalette = self.lcdCounter.palette()
         self.lcdCounter.setFixedSize(1350, 750)
         self.lcdCounter.setSmallDecimalPoint(False)
         self.lcdCounter.setDigitCount(5)
@@ -112,18 +123,18 @@ class Ui_MainWindow(object):
         self.lcdCounter.hide()
         self.runTime = ""
 
-        self.timer, self.glas_not_set_timer, self.glas_set_timer = QtCore.QTimer(), QtCore.QTimer(), QtCore.QTimer()
+        self.timer, self.glass_not_set_timer, self.glass_set_timer = QtCore.QTimer(), QtCore.QTimer(), QtCore.QTimer()
         self.timer.setInterval(100)
         self.timer.timeout.connect(self.tick_timer)
         self.timer.timeout.connect(self.stop_timer)
 
-        self.glas_set_timer.setSingleShot(True)
-        self.glas_set_timer.setInterval(100)
-        self.glas_set_timer.timeout.connect(self.glas_set)
+        self.glass_set_timer.setSingleShot(True)
+        self.glass_set_timer.setInterval(100)
+        self.glass_set_timer.timeout.connect(self.glass_set)
 
-        self.glas_not_set_timer.setSingleShot(True)
-        self.glas_not_set_timer.setInterval(100)
-        self.glas_not_set_timer.timeout.connect(self.glas_not_set)
+        self.glass_not_set_timer.setSingleShot(True)
+        self.glass_not_set_timer.setInterval(100)
+        self.glass_not_set_timer.timeout.connect(self.glass_not_set)
 
         self.gridPictures = QtWidgets.QGridLayout()
         self.gridPictures.addWidget(self.lcdCounter, 1, 1, QtCore.Qt.AlignCenter)
@@ -135,7 +146,8 @@ class Ui_MainWindow(object):
         self.hboxButtons.addWidget(self.startButton)
         self.hboxButtons.addWidget(self.toggleButton)
         self.hboxButtons.addWidget(self.highscoreButton)
-        self.hboxButtons.addWidget(self.cancelButton, QtCore.Qt.AlignRight)
+        self.hboxButtons.addWidget(self.cancelTimerButton)
+        self.hboxButtons.addWidget(self.cancelScoreButton)
 
         self.input_layout.addRow("", self.input_dialogue)
         self.input_layout.addRow("", self.inputButton)
@@ -152,7 +164,7 @@ class Ui_MainWindow(object):
         self.input_dialogue.returnPressed.connect(self.inputButton.click)
         self.inputButton.clicked.connect(lambda: self.update_scores(self.input_dialogue.text(), self.runTime))
         self.inputButton.clicked.connect(lambda: self.input_window.close())
-        self.inputButton.clicked.connect(lambda: self.exit_function())
+        self.inputButton.clicked.connect(lambda: self.exit_timer_function())
         self.centralwidget.setLayout(self.vbox)
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -179,10 +191,14 @@ class Ui_MainWindow(object):
         self.highscoreButton.setText(_translate("KnockOutMachine", "Bestenliste"))
         self.highscoreButton.setStyleSheet("background-color: white;")
         # self.cancelButton.setText(_translate("KnockOutMachine", "Abbrechen"))
-        self.cancelButton.setIcon(QtGui.QIcon("display/cancel_button.png"))
-        self.cancelButton.setIconSize(QtCore.QSize(50, 50))
-        self.cancelButton.setStyleSheet("background-color: white;")
-        self.lcdCounter.setStyleSheet("background-color: white;")
+        self.cancelTimerButton.setIcon(QtGui.QIcon("display/cancel_button.png"))
+        self.cancelTimerButton.setIconSize(QtCore.QSize(50, 50))
+        self.cancelTimerButton.setStyleSheet("background-color: white;")
+        self.cancelScoreButton.setIcon(QtGui.QIcon("display/cancel_button.png"))
+        self.cancelScoreButton.setIconSize(QtCore.QSize(50, 50))
+        self.cancelScoreButton.setStyleSheet("background-color: white;")
+        self.lcdCounter.setStyleSheet("background-color: #113f0c;")
+        self.lcdCounter.setStyleSheet("color: white;")
         self.tableview.setStyleSheet("background-color: white;")
 
     def enable_input_button(self):
@@ -196,32 +212,32 @@ class Ui_MainWindow(object):
         self.movie = QtGui.QMovie("display/dog.gif")
         self.lcdCounter.setEnabled(True)
         self.lcdCounter.show()
-        self.cancelButton.show()
+        self.cancelTimerButton.show()
         self.toggleButton.show()
         self.startButton.hide()
         self.highscoreButton.hide()
         self.pictures.hide()
 
         if Input_I1:
-            self.glas_not_set()
+            self.glass_not_set()
         else:
-            self.glas_set()
+            self.glass_set()
 
     def on_high_score_button_clicked(self):
         self.highscoreButton.hide()
+        self.startButton.setDisabled(True)
         self.startButton.hide()
         self.pictures.hide()
-        self.cancelButton.show()
+        self.cancelScoreButton.show()
         self.tableview.show()
         self.model.clear()
         self.model.setHorizontalHeaderLabels(['Name', 'Zeit in Sekunden'])
         self.model.setHeaderData(0, QtCore.Qt.Horizontal, QtCore.Qt.AlignJustify, QtCore.Qt.TextAlignmentRole)
-
-        DELIMITER = ';' if locale.localeconv()['decimal_point'] == ',' else ','
+        self.delimiter = ';'
 
         try:
             with open('timeList.csv', 'r') as timeFile:
-                reader = csv.reader(timeFile, delimiter=DELIMITER)
+                reader = csv.reader(timeFile, delimiter=self.delimiter)
                 reader = [[x.replace(',', '.') for x in l] for l in reader]
                 highscore_list_sorted = sorted(reader, key=lambda x: float(x[1]))[:10]
 
@@ -235,20 +251,20 @@ class Ui_MainWindow(object):
         except FileNotFoundError:
             print("Timelist File does not exist")
 
-    def glas_not_set(self):
-        self.glas_not_set_timer.start()
+    def glass_not_set(self):
+        self.glass_not_set_timer.start()
         self.messages.show()
         self.messages.setText("Bitte Glas vor Sensor stellen!")
         if not Input_I1:
-            self.glas_not_set_timer.stop()
-            self.glas_set()
+            self.glass_not_set_timer.stop()
+            self.glass_set()
 
-    def glas_set(self):
-        self.glas_set_timer.start()
+    def glass_set(self):
+        self.glass_set_timer.start()
         self.messages.show()
         self.messages.setText("Glas erkannt, wenn bereit los!")
         if Input_I1:
-            self.glas_set_timer.stop()
+            self.glass_set_timer.stop()
             self.movie.start()
             self.start_timer()
 
@@ -277,7 +293,7 @@ class Ui_MainWindow(object):
         self.runTime = "%02d.%02d" % (self.now / 100, self.now % 100)
         self.lcdCounter.display(self.runTime)
         if self.now / 100 == 99:
-            self.exit_function()
+            self.exit_timer_function()
 
     def tick_timer(self):
         self.now += 1
@@ -285,11 +301,11 @@ class Ui_MainWindow(object):
 
     def update_scores(self, inputName, runTime):
         self.datetime = QtCore.QDateTime.currentDateTime()
-        DELIMITER = ';' if locale.localeconv()['decimal_point'] == ',' else ','
+        self.delimiter = ';'
         row = [inputName, runTime.replace(".", ","), self.datetime.toString(QtCore.Qt.DefaultLocaleShortDate)]
 
         with open('timeList.csv', 'a', newline='') as timeFile:
-            writer = csv.writer(timeFile, delimiter=DELIMITER)
+            writer = csv.writer(timeFile, delimiter=self.delimiter)
             writer.writerow(row)
         self.input_dialogue.clear()
 
@@ -310,7 +326,7 @@ class Ui_MainWindow(object):
                 self.movie = QtGui.QMovie("display/dog.gif")
             self.movie.start()
             self.pictures.setMovie(self.movie)
-            self.play_sound("applause-2.mp3")
+            self.play_sound("cheering.mp3")
 
         elif runTime <= 500:
             self.rand = randint(0, 6)
@@ -329,7 +345,7 @@ class Ui_MainWindow(object):
                 self.movie = QtGui.QMovie("display/6.gif")
             self.movie.start()
             self.pictures.setMovie(self.movie)
-            self.play_sound("applause-8.mp3")
+            self.play_sound("applause-2.mp3")
 
         elif runTime <= 800:
             self.rand = randint(0, 2)
@@ -348,18 +364,32 @@ class Ui_MainWindow(object):
             self.pictures.setMovie(self.movie)
             self.play_sound("laughter-2.mp3")
 
-    def exit_function(self):
+    def exit_score_function(self):
+        self.tableview.hide()
+        self.toggleButton.hide()
+        self.cancelScoreButton.hide()
+        self.highscoreButton.show()
+        self.startButton.setEnabled(True)
+        self.startButton.show()
+        self.pictures.show()
+
+        self.pixmap = QtGui.QPixmap("display/main_menu.png")
+        self.pictures.setPixmap(self.scaledPixmap)
+
+    def exit_timer_function(self):
         # self.rpi.exit(full=False)
         self.timer.stop()
-        self.glas_set_timer.stop()
-        self.glas_not_set_timer.stop()
+        self.glass_set_timer.stop()
+        self.glass_not_set_timer.stop()
         self.player.stop()
+        self.lcdCounter.setEnabled(False)
         self.lcdCounter.hide()
         self.tableview.hide()
         self.toggleButton.hide()
-        self.cancelButton.hide()
+        self.cancelTimerButton.hide()
         self.messages.hide()
         self.highscoreButton.show()
+        self.startButton.setEnabled(True)
         self.startButton.show()
         self.pictures.show()
 
